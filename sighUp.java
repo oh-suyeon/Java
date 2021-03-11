7214692d676c658aa8695e96a56259a8a27d0140
DESKTOP-IUJNLEG\PC-08 <djs02061@naver.com>
2021-03-11 19:33:55 +0900

    변수 만들기 / 입출력하기 복습

--- sighUp.java
+++ sighUp.java
@@ -1,9 +1,10 @@
 package study;
 
+import java.util.Scanner;
+
 public class sighUp {
 
 	public static void main(String[] args) {
-		
 		
 		/*
 		 * ========== 회원가입 ==========
@@ -22,7 +23,45 @@ public class sighUp {
 		 * 키 : 185.5cm
 		 * ===========================
 		 */
+		
+		Scanner sc = new Scanner(System.in);
 
+		String id;
+		int password;
+		String name;
+		int age;
+		double height;
+		
+		System.out.println("========== 회원가입 ==========");
+		
+		System.out.println("아이디를 입력하세요 >");
+		id = sc.nextLine();
+		System.out.println("비밀번호 네 자리를 입력하세요 >");
+		password = Integer.parseInt(sc.nextLine());
+		System.out.println("이름을 입력하세요 >");
+		name = sc.nextLine();
+		System.out.println("나이를 입력하세요 >");
+		age = Integer.parseInt(sc.nextLine());
+		System.out.println("키를 입력하세요 >");
+		height = Double.parseDouble(sc.nextLine());
+		
+		System.out.println("===========================");
+		System.out.println("회원가입 완료!!");
+		System.out.println("===========================");
+		
+		System.out.println("아이디 :" + id);
+		System.out.println("비밀번호 :" + password);
+		System.out.println("이름 :" + name);
+		System.out.println("나이 :" + age + "세");
+		System.out.println("키 :" + height + "cm");
+		
+		System.out.println("===========================");
+		
+		
+		
+		
+		
+		
 	}
 
 }

66a04a0db04737100b85cf139edb288c9ad8f5b9
DESKTOP-IUJNLEG\PC-08 <djs02061@naver.com>
2021-03-15 21:27:01 +0900

    - if 조건문 - 탄수화물 중독 자가진단 테스트 코딩

---
+++ SelfTest.java
@@ -0,0 +1,52 @@
+package study;
+
+import java.util.Scanner;
+
+public class SelfTest {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("탄수화물 중독 자가진단 테스트입니다.");
+		System.out.println("해당되는 항목에는 1을, 그렇지 않은 항목에는 0을 입력해주세요.");
+		System.out.println("*****************************************");
+		System.out.println("아침을 배불리 먹은 후 점심시간 전에 배가 고프다.");
+		int a = Integer.parseInt(sc.nextLine());
+
+		System.out.println("밥, 빵 과자 등 음식을 먹기 시작하면 끝이 없다.");
+		int b = Integer.parseInt(sc.nextLine());
+		System.out.println("음식을 금방 먹은 후에도 만족스럽지 못하고 더 먹는다.");
+		int c = Integer.parseInt(sc.nextLine());
+		System.out.println("정말 배고프지 않더라도 먹을 때가 있다.");
+		int d = Integer.parseInt(sc.nextLine());
+		System.out.println("저녁을 먹고 간식을 먹지 않으면 잠이 오지 않는다.");
+		int e = Integer.parseInt(sc.nextLine());
+		System.out.println("스트레스를 받으면 자꾸 먹고 싶어진다.");
+		int f = Integer.parseInt(sc.nextLine());
+		System.out.println("책상이나 식탁 위에 항상 과자, 초콜릿 등이 놓여있다.");
+		int g = Integer.parseInt(sc.nextLine());
+		System.out.println("오후 5시가 되면 피곤함과 배고픔을 느끼고 일이 손에 안 잡힌다.");
+		int h = Integer.parseInt(sc.nextLine());
+		System.out.println("과자, 초콜릿 등 단 음식은 상상만해도 먹고 싶어진다.");
+		int r = Integer.parseInt(sc.nextLine());
+		System.out.println("다이어트를 위해 식이조절을 하는데 3일도 못 간다.");
+		int j = Integer.parseInt(sc.nextLine());
+
+		int sum = a + b + c + d + e + f + g + h + r + j;
+		
+		System.out.println("*****************************************");
+		if (sum >= 7) {
+			System.out.println("당신의 점수는 " + sum + "점 입니다.\n" + "중독! 전문의 상담이 필요합니다.");
+		} else if (sum == 3) {
+			System.out.println("당신의 점수는 " + sum + "점 입니다.\n" + "주의! 위험한 수준은 아니지만 관리가 필요합니다.");
+		} else if (sum < 3) {
+			System.out.println("당신의 점수는 " + sum + "점 입니다.\n" + "탄수화물을 건강하게 섭취하고 있습니다.");
+		} else {
+			System.out.println("당신의 점수는 " + sum + "점 입니다.\n" + "위험! 탄수화물 섭취를 줄이기 위한 식습관 개선이 필요합니다.");
+		}
+		
+		
+	}
+
+}
